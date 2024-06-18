3e83d46fb16da39582c402fd5dacaf25e71bb554
justnico69 <andina.aljo@gmail.com>
2024-06-18 19:25:00 +0800

    Rejection Mail Implementation

--- pit/app/Http/Controllers/ApplicantsController.php
+++ pit/app/Http/Controllers/ApplicantsController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\AdmissionInfo;
+use App\Models\RejectedApplicants;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\RejectedApplicant;
 
 class ApplicantsController extends Controller
 {
     public function index()
     {
-        $users = AdmissionInfo::select('id', 'firstName', 'lastName')->get(); // Make sure 'id' is selected
+        $users = AdmissionInfo::select('id', 'firstName', 'lastName')->get();
         $names = $users->map(function ($user) {
             return [
                 'id' => $user->id,
@@ -24,4 +27,39 @@ class ApplicantsController extends Controller
         $user = AdmissionInfo::find($id);
         return response()->json($user);
     }
+
+    public function reject($id)
+    {
+        $applicant = AdmissionInfo::find($id);
+
+        if (!$applicant) {
+            return response()->json(['message' => 'Applicant not found'], 404);
+        }
+
+        $rejectedApplicant = new RejectedApplicants();
+        $rejectedApplicant->firstName = $applicant->firstName;
+        $rejectedApplicant->middleName = $applicant->middleName;
+        $rejectedApplicant->lastName = $applicant->lastName;
+        $rejectedApplicant->month = $applicant->month;
+        $rejectedApplicant->day = $applicant->day;
+        $rejectedApplicant->year = $applicant->year;
+        $rejectedApplicant->gender = $applicant->gender;
+        $rejectedApplicant->nationality = $applicant->nationality;
+        $rejectedApplicant->email = $applicant->email;
+        $rejectedApplicant->contactno = $applicant->contactno;
+        $rejectedApplicant->streetadd = $applicant->streetadd;
+        $rejectedApplicant->city = $applicant->city;
+        $rejectedApplicant->province = $applicant->province;
+        $rejectedApplicant->zipcode = $applicant->zipcode;
+        $rejectedApplicant->emergencyName = $applicant->emergencyName;
+        $rejectedApplicant->relationship = $applicant->relationship;
+        $rejectedApplicant->emergencyContactNumber = $applicant->emergencyContactNumber;
+        $rejectedApplicant->schoolLastAttended = $applicant->schoolLastAttended;
+        $rejectedApplicant->save();
+
+        $applicant->delete();
+
+        return response()->json(['message' => 'Applicant rejected successfully'], 200);
+    }
 }
+
