4d3d4d7884c39d2afe296702196e158fb0a2393e
Nics-IT2R2 <160290934+Nics-IT2R2@users.noreply.github.com>
2024-06-22 00:51:02 +0800

    Department sidebar changes

--- pit/resources/js/Pages/DepartmentComponents/TCM/TcmHeader.jsx
+++ pit/resources/js/Pages/DepartmentComponents/TCM/TcmHeader.jsx
@@ -1,26 +1,24 @@
-import React from 'react';
-
-function TcmHeader() {
+export default function Header() {
   return (
-    <header>
-      <nav className="bg-white">
-        <div className="text-blue-800 px-10 py-6 z-10 w-full border-blue-200">
-          <div className="flex items-center justify-between py-2 text-xl">
-            <img
-              src="https://i.ibb.co/Q6SrF6M/Edutech.png"
-              alt="EduTech Logo"
-              className="h-8 w-30 object-contain mr-5 mb-3-2"
-            />
-            <span className="text-xl font-bold">
-              NNN College of Information Technology and Computing Admission Form
-            </span>
-            <div className="flex items-center ml-auto text-gray-500">
-            </div>
-          </div>
-        </div>
-      </nav>
-    </header>
+      <header>
+          <nav className="fixed bg-white flex w-full z-50 shadow-md items-center justify-between">
+              <div className="text-blue-800 px-10 py-5 w-full">
+                  <div className="flex items-center justify-between py-2 text-5x1">
+                      <div className="flex items-center justify-center">
+                          <img
+                              src="https://i.ibb.co/Q6SrF6M/Edutech.png"
+                              alt="EduTech Logo"
+                              className="h-8 w-30 object-contain mr-5 mb-3-2"
+                          />
+                          <div className="text-blue-600 text-xl">
+                              <div className="font-bold mb-2 mt-2">
+                                  NNN<span className="text-blue-800"> College of Information Technology and Computing Enrollment Portal</span>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </nav>
+      </header>
   );
 }
-
-export default TcmHeader;
