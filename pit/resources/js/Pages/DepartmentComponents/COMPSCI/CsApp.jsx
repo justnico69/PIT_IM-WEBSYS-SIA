866b66ad96425151c98d92cb417f450b4d000d3a
Nics-IT2R2 <160290934+Nics-IT2R2@users.noreply.github.com>
2024-06-22 14:12:30 +0800

    CS Changes

--- pit/resources/js/Pages/DepartmentComponents/COMPSCI/CsApp.jsx
+++ pit/resources/js/Pages/DepartmentComponents/COMPSCI/CsApp.jsx
@@ -1,11 +1,14 @@
+// App.jsx
 import React from 'react';
 import CsHeader from './CsHeader';
-import CsMainCont from './CsMainCont';
+import CsSidebar from './CsSidebar';
+import CsMainCont from './CsMainCont'; // Update import
 
 function App() {
   return (
-    <div className="min-h-screen w-full" style={{ backgroundImage: 'url(https://img.freepik.com/free-photo/vivid-blurred-colorful-wallpaper-background_58702-3920.jpg?t=st=1718357895~exp=1718361495~hmac=e029c64e2bf40ce2b07c6f1ad9b35cf47c271e526e50a0e4c19e99d9193c60e8&w=900)', backgroundPosition: 'top', backgroundSize: 'cover' }}>
+    <div className="min-h-screen w-full flex" style={{ backgroundImage: 'url(https://img.freepik.com/free-photo/vivid-blurred-colorful-wallpaper-background_58702-3920.jpg?t=st=1718357895~exp=1718361495~hmac=e029c64e2bf40ce2b07c6f1ad9b35cf47c271e526e50a0e4c19e99d9193c60e8&w=900)', backgroundPosition: 'top', backgroundSize: 'cover' }}>
       <CsHeader />
+      <CsSidebar />
       <CsMainCont />
     </div>
   );
